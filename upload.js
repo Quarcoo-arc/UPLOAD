a8cded74e5a93b179460047a273936058febee9f
Michael Quarcoo <michaelquarcoo04@gmail.com>
2021-10-31 21:05:58 +0000

    File upload API

--- upload.js
+++ upload.js
@@ -7,7 +7,6 @@ const Grid = require("gridfs-stream");
 const path = require("path");
 const crypto = require("crypto");
 const methodOverride = require("method-override");
-const File = require("./model/fileSchema");
 
 const exp = express();
 const bodyParser = body_parser;
@@ -22,44 +21,137 @@ const connection = mongoose.createConnection(URI);
 
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION, APP SHUTTING DOWN!");
-  console.log(err.name, err.name);
+  console.log(err.name, err.message);
   process.exit(1);
 });
 
-let gfs;
-
-connection.once("open", () => {
-  gfs = Grid(connection.db, mongoose.mongo);
-  gfs.collection("uploads");
-  console.log("Database connected");
-});
-
-const storage = new GridFsStorage({
-  url: URI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString("hex") + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: "uploads",
-        };
-        resolve(fileInfo);
-        console.log("File added successfully");
-      });
-    });
+// Creating a Schema for uploaded files
+const fileSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: [true, "Uploaded file must have a name"],
   },
 });
 
+// Creating a Model from that Schema
+const File = connection.model("File", fileSchema);
+
+// let gfs;
+
+connection.once("open", () => {
+  // gfs = Grid(connection.db, mongoose.mongo);
+  // gfs.collection("uploads");
+  console.log("Database connected");
+});
+
+// const storage = new GridFsStorage({
+//   url: URI,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         const filename = buf.toString("hex") + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: "uploads",
+//         };
+//         resolve(fileInfo);
+//         console.log("File added successfully");
+//       });
+//     });
+//   },
+// });
+
 //Configuration for multer
-const upload = multer({ dest: "public/files" });
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`);
+  },
+});
+
+//Array of acceptable file types
+const fileTypes = ["pdf", "jpg", "jpeg", "png"];
+
+// Multer Filter
+const multerFilter = (req, file, cb) => {
+  if (fileTypes.includes(file.mimetype.split("/")[1])) {
+    cb(null, true);
+  } else {
+    cb(new Error("File Type Not Supported!!"), false);
+  }
+};
+
+//Calling the "multer" Function
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
 //API Endpoint for uploading file
-exp.post("/upload", upload.single("file"), (req, res) => {
-  console.log({ file: req.file });
+exp.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    const fileUploaded = req.file;
+    console.log({ file: fileUploaded });
+    if (!fileUploaded) {
+      return res.send("File not Foune!");
+    } else {
+      const newFile = await File.create(
+        {
+          name: req.file.filename,
+        },
+        (err, file) => {
+          if (!err) console.log("File Successfully saved!");
+          else console.log("Error saving file to database: ", err);
+        }
+      );
+      return res.send("File uploaded successfully");
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+    res.send("Error Adding File!");
+  }
+  // try {
+  //   const newFile = await File.create({
+  //     name: req.file.filename,
+  //   });
+  //   res.status(200).json({
+  //     status: "success",
+  //     message: "File created successfully!!",
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.json({
+  //     error,
+  //   });
+  // } finally {
+  //   res.end();
+  // }
+});
+
+//Endpoint for viewing uploads on the frontend
+exp.get("/upload/files", async (req, res) => {
+  try {
+    const files = await File.find();
+    res.status(200).json({
+      status: "success",
+      files,
+    });
+  } catch (error) {
+    res.json({
+      status: "Fail",
+      error,
+    });
+  }
 });
 
 // exp.get('/files', (req, res) => {
